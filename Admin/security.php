25355e9a1f3d67c714e5800e67dc251c63d2121a
ibraloyza <ibraahim3523@gmail.com>
2024-10-07 17:12:39 +0300

    added  a security page

---
+++ Admin/security.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+include('../dbcon.php');
+
+if($conn)
+{
+    // echo "database connected"
+}
+else
+{
+    header('location:../dbcon.php');
+}
+if(!$_SESSION['username'])
+{
+    header('location:login.php');
+}
+
+?>
